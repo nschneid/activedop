3e774be1142111383b89b16d80ab17b8a3800300
Brandon Waldon <32781952+bwaldon@users.noreply.github.com>
2024-08-27 12:47:50 -0400

    clarify conditional logic in tree_process and remove superfluous escaping

--- app.py
+++ app.py
@@ -881,15 +881,12 @@ def tree_process(tree : ParentedTree, senttok: List[str]) -> tuple[ParentedTree,
 		# if initial parse labels non-gaps as GAP, change to N-Head by default
 		if subt.label.startswith('GAP') and senttok[i] != '_.':
 			subt.label = 'N-Head'
-		# condition 1: label is a punctuation sequence w/o -p. (occurs when a ctree terminal is re-analyzed as a :p tag of another terminal and ctree is re-converted to ptree)
-		# condition 2: label is punctuation sequence + "-p" 
+		# condition 1: label is a punctuation sequence w/o -p. [e.g, the label in node `(, ,)`]. Can occur in `annotate` when tree_process() receives an initial dopparsed tree, and in `edit` when tree_process() receives a ParentedTree-format PTB-converted ctree. 
+		# condition 2: label is punctuation sequence + "-p"
 		# condition 3: token is unabiguously punctuation
 		# -> assign the appropriate punctuation label (either from PUNCT_TAGS or the default SYMBOL_TAG)
 		if is_possible_punct_token(subt.label) or is_punct_label(subt.label) or (is_possible_punct_token(senttok[i]) and senttok[i] not in AMBIG_SYM):
 			subt.label = PUNCT_TAGS.get(senttok[i], SYMBOL_TAG) + "-p"
-		for e in PUNCT_ESCAPING:
-			if subt.label == e['ptree_token']:
-				subt.label = e['ptree_label']
 			# if initial parse labels non-punctuation as punctuation, change to N-Head
 		if (not is_possible_punct_token(senttok[i])) and (is_punct_label(subt.label)):
 			subt.label = 'N-Head'
